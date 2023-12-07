cdcb7c86b14e8ce809049ca66f55490008f06284
JiHongKim98 <kinjihong9598@gmail.com>
2023-12-08 03:40:57 +0900

    feat: Add comment model #5
    The first task in Issue #5 is completed.
    This task involved implementing Comment Model.
    This model includes defining the 'owner' fields as a Foreign Key
    to the User Model and 'board' fields as a Foreign Key to the Board
    Model.

--- boards/models.py
+++ boards/models.py
@@ -18,3 +18,20 @@ class PostModel(models.Model):
     class Meta:
         # 역순으로 정렬
         ordering = ['-id']
+
+
+# 댓글 모델
+class CommentModel(models.Model):
+    # FK (User 모델, Board 모델과의 관계 설정)
+    owner = models.ForeignKey(User, on_delete= models.CASCADE, related_name= "comment_owner")
+    board = models.ForeignKey(PostModel, on_delete= models.CASCADE, related_name= "post_comment")
+
+    contents = models.TextField(null= False)
+    created_date = models.DateTimeField("작성일", auto_now_add=True, null=False)
+    updated_date = models.DateTimeField("마지막 수정일", auto_now=True, null=False)
+
+    def __str__(self) -> str:
+        return f"{self.owner} 님의 댓글"
+    
+    class Meta:
+        ordering = ['-id']
