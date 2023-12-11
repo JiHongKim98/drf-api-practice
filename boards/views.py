3b1f2367bc4ccea696f1ea60b99d5024e0112bdd
JiHongKim98 <kinjihong9598@gmail.com>
2023-12-11 21:50:49 +0900

    refactor: Modify accounts and boards view #6
    Improved comments for better readability and understanding.
    Enhanced consistency by updating the method of receiving the 'pk'
    for the CREATE action in the comment APIView.
    
    Closes #6

--- boards/views.py
+++ boards/views.py
@@ -12,119 +12,95 @@ from .permissions import IsOwnerOrReadOnly
 
 # Post API (게시글 API)
 class PostAPIView(APIView):
-    # 권한처리
-    permission_classes = [IsOwnerOrReadOnly]
-
-    # 토큰 또는 세션 을 통한 인증
-    authentication_classes = [JWTAuthentication]
-
-    # 페이징 처리
-    pagination_class = pagination.PageNumberPagination
+    permission_classes = [IsOwnerOrReadOnly] # 권한처리
+    authentication_classes = [JWTAuthentication] # 토큰 또는 세션 을 통한 인증
+    pagination_class = pagination.PageNumberPagination # 페이징 처리
 
     # Read (CRUD 의 READ 부분)
     def get(self, request, pk=None):
-        if pk is None:
-            # PostModel 내 저장된 모든 게시글 데이터
-            queryset = PostModel.objects.all()
-            page = self.pagination_class()
-            Post_data_page = page.paginate_queryset(queryset, request)
-
-            # 데이터 일부분만 보여주는 쿼리문 => URI?page=1
-            if Post_data_page is not None:
-                serializer = PostModelSerializer(Post_data_page, many=True)
-                return page.get_paginated_response(serializer.data)
-            
-            serializer = PostModelSerializer(queryset, many=True)
-
+        if pk:
+            return self.retrieve_posts(request, pk) # pk exist => 게시글 세부사항
         else:
-            # 해당 PK를 가진 "게시글" 데이터과 "댓글" 목록을 가져옴
-            queryset = get_object_or_404(PostModel, id= pk)
-            serializer = PostModelSerializer(instance= queryset)
-            comment_queryset = queryset.post_comment.all() # related_name 으로 역참조
-            comment_serializer = CommentModelSerializer(instance= comment_queryset, many= True)
-
-            data = {
-                "board": serializer.data,
-                "comment": comment_serializer.data
-            }
-            return Response(data, status= status.HTTP_200_OK)
-
-        return Response(serializer.data, status= status.HTTP_200_OK)
+            return self.list_posts(request) # pk none => 게시글 목록
     
 
     # Create (CRUD 의 CREATE 부분)
     def post(self, request, pk=None):
-        # Create는 pk 정보가 없을때만 작동
-        if pk is None:
-            # serializer에 데이터를 보낼 때, request.data 에
-            # 사용자 정보(pk)를 추가해서 처리 
-            # 이유 => owner는 외래키(fk)로 User 모델과 연결 되어있음.
-            user = request.user
-            new_data = {**request.data, 'owner': user.pk}
-            print(new_data)
-            serializer = PostModelSerializer(data= new_data)
-
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status= status.HTTP_201_CREATED)
-
-            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
-        
-        else:
+        if pk:
             return Response(status= status.HTTP_400_BAD_REQUEST)
+
+        # owner(pk) 정보를 추가하여 serializer를 반환
+        user = request.user
+        new_data = {**request.data, 'owner': user.pk}
+        serializer = PostModelSerializer(data= new_data)
+
+        return self.handle_response(serializer)
         
     
     # Upate - 해당하는 데이터의 "일부"만 업데이트 (CRUD 의 UPDATE 부분)
     def patch(self, request, pk=None):
-        if pk is not None:
-            queryset = get_object_or_404(PostModel, id= pk)
-
-            # object의 접근 권한 확인 (해당 게시글을 작성한 사람만 가능)
-            self.check_object_permissions(request= request, obj= queryset)
-            serializer = PostModelSerializer(instance= queryset, data= request.data, partial= True)
-
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status= status.HTTP_200_OK)
-
-            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
-        
-        else:
-            return Response(status= status.HTTP_400_BAD_REQUEST)
+        return self.partial_update(request, pk, partial= True)
         
 
     # Upate - 해당하는 데이터의 "전체"를 업데이트 (CRUD 의 UPDATE 부분)
     def put(self, request, pk=None):
-        if pk is not None:
-            queryset = get_object_or_404(PostModel, id= pk)
-
-            # object의 접근 권한 확인 (해당 게시글을 작성한 사람만 가능)
-            self.check_object_permissions(request= request, obj= queryset)
-            serializer = PostModelSerializer(instance= queryset, data= request.data)
-
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status= status.HTTP_200_OK)
-
-            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
-        
-        else:
-            return Response(status= status.HTTP_400_BAD_REQUEST)
+        return self.partial_update(request, pk, partial= False)
         
 
     # Delete - (CRUD 의 DELETE 부분)
     def delete(self, request, pk=None):
-        if pk is not None:
-            queryset = get_object_or_404(PostModel, id= pk)
-
-            # object의 접근 권한 확인 (해당 게시글을 작성한 사람만 가능)
-            self.check_object_permissions(request= request, obj= queryset)
-            queryset.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        
-        else:
+        if pk is None:
             return Response(status= status.HTTP_400_BAD_REQUEST)
         
+        queryset = get_object_or_404(PostModel, id= pk)
+        self.check_object_permissions(request= request, obj= queryset) # 해당 게시글을 작성한 사람만 가능
+        queryset.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+    # pk none => 게시글 목록
+    def list_posts(self, request):
+        queryset = PostModel.objects.all()
+        page = self.pagination_class()
+        post_data_page = page.paginate_queryset(queryset, request)
+
+        # 게시글 페이징 처리
+        serializer = PostModelSerializer(post_data_page, many= True) if post_data_page else PostModelSerializer(queryset, many= True)
+        return self.handle_response(serializer, page, post_data_page)
+
+    # pk exist => 게시글 세부사항
+    def retrieve_posts(self, request, pk):
+        post_instance = get_object_or_404(PostModel, id= pk)
+        serializer = PostModelSerializer(post_instance)
+        comment_queryset = post_instance.post_comment.all() # related_name 으로 역참조
+        comment_serializer = CommentModelSerializer(comment_queryset, many= True)
+
+        data = {"board": serializer.data, "comment": comment_serializer.data}
+        return Response(data, status=status.HTTP_200_OK)
+    
+    # PUT 과 PATCH 에서의 중복된 코드를 하나로 통일 (중복 코드 개선)
+    def partial_update(self, request, pk=None, partial=True):
+        if pk is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        queryset = get_object_or_404(PostModel, id=pk)
+        self.check_object_permissions(request=request, obj=queryset) # 해당 게시글을 작성한 사람만 가능
+        serializer = PostModelSerializer(instance= queryset, data= request.data, partial= partial)
+        return self.handle_response(serializer)
+    
+    # 유효성 검사와 저장후 응답을 위한 핸들러 (중복 코드 개선)
+    def handle_response(self, serializer, page=None, paginated_queryset=None):
+        if serializer.is_valid():
+            serializer.save()
+
+            # page가 있다면 page 처리응답
+            if page:
+                return page.get_paginated_response(serializer.data) if paginated_queryset else Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 # comment API (댓글 API)
 class CommentAPIView(APIView):
@@ -132,59 +108,65 @@ class CommentAPIView(APIView):
     authentication_classes = [JWTAuthentication]
 
 
-    def get(self, request, pk):
+    def get(self, request, pk=None):
+        if pk is None:
+            return Response(status= status.HTTP_400_BAD_REQUEST)
+        
         queryset = get_object_or_404(CommentModel, id= pk)
         serializer = CommentModelSerializer(instance= queryset)
-
         return Response(serializer.data, status= status.HTTP_200_OK)
 
 
-    # POST 메소드는 해당 게시글(post)의 pk를 request로 받아서
-    # 게시글에 댓글을 저장시 CommentModel에서 PostModel을 참조하고 있는
-    # board 필드를 지정해서 저장하게 끔 구현
-    def post(self, request, pk):
-        try:
-            new_data = {**request.data, 'board': pk, 'owner': request.user.pk}
-            serializer = CommentModelSerializer(data= new_data)
-            serializer.is_valid(raise_exception= True)
-            serializer.save()
-
-            return Response(serializer.data, status= status.HTTP_201_CREATED)
-        except Exception as e:
-            print(e)
+    def post(self, request, pk=None):
+        if pk:
             return Response(status= status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            # owner(pk) 정보를 추가하여 serializer를 반환
+            new_data = {**request.data, 'owner': request.user.pk}
+            serializer = CommentModelSerializer(data= new_data)
+            self.valid_and_save(serializer)
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        
+        except Exception as e:
+            return Response({"error": str(e)}, status= status.HTTP_400_BAD_REQUEST)
 
 
-    # PATCH 메소드는 댓글의 PK 를 받는다.
-    def patch(self, request, pk):
-        queryset = get_object_or_404(CommentModel, id= pk)
-        # object의 접근 권한 확인
-        self.check_object_permissions(request= request, obj= queryset)
-
-        serializer = CommentModelSerializer(instance= queryset, data= request.data, partial= True)
-        serializer.is_valid(raise_exception= True)
-        serializer.save()
-
-        return Response(serializer.data, status= status.HTTP_200_OK)
+    def patch(self, request, pk=None):
+        return self.partial_update(request, pk, partial= True)
 
 
-    def put(self, request, pk):
-        queryset = get_object_or_404(CommentModel, id= pk)
-        # object의 접근 권한 확인
-        self.check_object_permissions(request= request, obj= queryset)
-
-        serializer = CommentModelSerializer(instance= queryset, data= request.data)
-        serializer.is_valid(raise_exception= True)
-        serializer.save()
-
-        return Response(serializer.data, status= status.HTTP_200_OK)
+    def put(self, request, pk=None):
+        return self.partial_update(request, pk, partial= False)
 
 
-    def delete(self, request, pk):
-        queryset = get_object_or_404(CommentModel, id= pk)
-        # object의 접근 권한 확인
-        self.check_object_permissions(request= request, obj= queryset)
+    def delete(self, request, pk=None):
+        if pk is None:
+            return Response(status= status.HTTP_400_BAD_REQUEST)
+        
+        queryset = self.get_comment_and_check_permission(request, pk)
         queryset.delete()
-
         return Response(status= status.HTTP_204_NO_CONTENT)
+    
+
+    # 유효성 검사후 save (중복 코드 개선)
+    def valid_and_save(self, serializer):
+        serializer.is_valid(raise_exception= True)
+        serializer.save()
+
+    # Comment 정보를 가져오고 권한 검사 (중복 코드 개선)
+    def get_comment_and_check_permission(self, request, pk):
+        queryset = get_object_or_404(CommentModel, id= pk)
+        self.check_object_permissions(request= request, obj= queryset) # object의 접근 권한 확인
+        return queryset
+
+    # PUT 과 PATCH 에서의 중복된 코드를 하나로 통일 (중복 코드 개선)
+    def partial_update(self, request, pk=None, partial=True):
+        if pk is None:
+            return Response(status= status.HTTP_400_BAD_REQUEST)
+        
+        queryset = self.get_comment_and_check_permission(request, pk)
+        serializer = CommentModelSerializer(instance= queryset, data= request.data, partial= partial)
+        self.valid_and_save(serializer)
+        return Response(serializer.data, status= status.HTTP_200_OK)
 
